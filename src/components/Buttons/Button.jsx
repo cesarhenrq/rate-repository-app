d06c879c6ccc793c8c3e97a288f2a93a101cc8b2
Cééesar Henrique <cesaarhvb@gmail.com>
2024-06-26 02:34:26 -0300

    chore: add Button component for reusable button functionality

---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,30 @@
+import { Pressable, StyleSheet } from "react-native";
+
+import Subheading from "../Texts/Subheading";
+
+import theme from "../../theme";
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: theme.colors.primary,
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+
+  buttonText: {
+    color: "white",
+  },
+});
+
+const Button = ({ onPress, text, marginTop }) => {
+  const buttonStyles = [styles.button, marginTop && { marginTop }];
+
+  return (
+    <Pressable onPress={onPress} style={buttonStyles}>
+      <Subheading style={styles.buttonText}>{text}</Subheading>
+    </Pressable>
+  );
+};
+
+export default Button;
