ebf39e2fbf681dd5826507568bf9fa4f942e53d6
Cééesar Henrique <cesaarhvb@gmail.com>
2024-06-26 02:34:21 -0300

    feat: add repository item component for individual repository view

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import { StyleSheet, View } from "react-native";
 import { Route, Routes, Navigate } from "react-router-native";
 
 import RepositoryList from "./RepositoryList";
+import RepositoryItem from "./RepositoryList/RepositoryItem";
 import AppBar from "./AppBar";
 import SignIn from "./SignIn";
 import SignOut from "./SignOut";
@@ -28,6 +29,7 @@ const Main = () => {
         <Route path='/' element={<RepositoryList />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signout' element={<SignOut />} />
+        <Route path='/repository/:id' element={<RepositoryItem />} />
         <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
     </View>
