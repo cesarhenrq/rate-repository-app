3e85a55c3c93d07b9d7bb7198890cfd5695acd47
Cééesar Henrique <cesaarhvb@gmail.com>
2024-06-26 02:34:33 -0300

    feat: update RepositoryListContainer to use Pressable for repository item navigation

--- src/components/RepositoryList/RepositoryListContainer.jsx
+++ src/components/RepositoryList/RepositoryListContainer.jsx
@@ -1,6 +1,8 @@
-import { FlatList, View, StyleSheet } from "react-native";
+import { FlatList, View, StyleSheet, Pressable } from "react-native";
 
-import RepositoryItem from "./RepositoryItem";
+import { useNavigate } from "react-router-native";
+
+import RepositoryItemContainer from "./RepositoryItem/RepositoryItemContainer";
 
 const styles = StyleSheet.create({
   separator: {
@@ -15,12 +17,16 @@ const RepositoryListContainer = ({ repositories }) => {
     ? repositories.edges.map((edge) => edge.node)
     : [];
 
+  const navigate = useNavigate();
+
   return (
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => (
-        <RepositoryItem key={item.id} repository={item} />
+        <Pressable onPress={() => navigate(`/repository/${item.id}`)}>
+          <RepositoryItemContainer key={item.id} repository={item} />
+        </Pressable>
       )}
     />
   );
