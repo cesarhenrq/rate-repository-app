c79e715a20030dd07fa636f72d699b84c9f809c9
Cééesar Henrique <cesaarhvb@gmail.com>
2024-06-27 23:50:04 -0300

    feat: Add CREATE_REVIEW mutation for creating new reviews

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -7,3 +7,25 @@ export const SIGN_IN = gql`
     }
   }
 `;
+
+export const CREATE_REVIEW = gql`
+  mutation createReview(
+    $repositoryName: String!
+    $ownerName: String!
+    $rating: Int!
+    $text: String
+  ) {
+    createReview(
+      review: {
+        repositoryName: $repositoryName
+        ownerName: $ownerName
+        rating: $rating
+        text: $text
+      }
+    ) {
+      repository {
+        id
+      }
+    }
+  }
+`;
