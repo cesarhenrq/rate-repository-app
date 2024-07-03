1af782326a8a848f491d9b34b8805446d0d1a32d
Cééesar Henrique <cesaarhvb@gmail.com>
2024-07-02 22:02:56 -0300

    feat: update GET_REPOSITORIES query to include orderBy and orderDirection variables

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -3,8 +3,11 @@ import { gql } from "@apollo/client";
 import { REPOSITORY_FIELDS } from "./fragments";
 
 export const GET_REPOSITORIES = gql`
-  query {
-    repositories {
+  query repositories(
+    $orderBy: AllRepositoriesOrderBy
+    $orderDirection: OrderDirection
+  ) {
+    repositories(orderBy: $orderBy, orderDirection: $orderDirection) {
       edges {
         node {
           ...RepositoryFields
