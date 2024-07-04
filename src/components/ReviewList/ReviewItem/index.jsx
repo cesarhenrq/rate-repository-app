2b64a39dde8b4c5969b5bf5b5002ac26b6b86360
Cééesar Henrique <cesaarhvb@gmail.com>
2024-07-03 23:15:22 -0300

    feat: add ReviewItem component to display individual reviews in ReviewList

---
+++ src/components/ReviewList/ReviewItem/index.jsx
@@ -0,0 +1,37 @@
+import { View, StyleSheet } from "react-native";
+
+import Rating from "./Rating";
+import Subheading from "../../Texts/Subheading";
+import Text from "../../Texts/Text";
+
+import formatDate from "../../../utils/formatDate";
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 10,
+    padding: 10,
+    backgroundColor: "white",
+  },
+
+  innerContainer: {
+    flexShrink: 1,
+  },
+});
+
+const ReviewItem = ({ review }) => {
+  return (
+    <View style={styles.container}>
+      <Rating rating={review.rating} />
+      <View style={styles.innerContainer}>
+        <Subheading>
+          {review?.repository?.fullName || review.user.username}
+        </Subheading>
+        <Text color='textSecondary'>{formatDate(review.createdAt)}</Text>
+        <Text>{review.text}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ReviewItem;
