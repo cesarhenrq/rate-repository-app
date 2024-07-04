59a24993cbde0511fc9f27551361183c4219df19
Cééesar Henrique <cesaarhvb@gmail.com>
2024-07-03 23:52:10 -0300

    feat: add delete functionality to ReviewList component

--- src/components/ReviewList/index.jsx
+++ src/components/ReviewList/index.jsx
@@ -2,15 +2,31 @@ import ReviewListContainer from "./ReviewListContainer";
 import Loading from "../Loading";
 
 import useMe from "../../hooks/useMe";
+import useDeleteReview from "../../hooks/useDeleteReview";
 
 const ReviewList = () => {
   const { data, loading } = useMe({ includeReviews: true });
+
+  const deleteReview = useDeleteReview();
+
+  const handleDeleteReview = async (id) => {
+    const result = await deleteReview(id);
+
+    if (result.data?.deleteReview) {
+      data.refetch();
+    }
+  };
 
   if (loading) {
     return <Loading />;
   }
 
-  return <ReviewListContainer reviews={data.me.reviews.edges} />;
+  return (
+    <ReviewListContainer
+      reviews={data.me.reviews.edges}
+      onDelete={handleDeleteReview}
+    />
+  );
 };
 
 export default ReviewList;
